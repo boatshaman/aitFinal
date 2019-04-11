5370a1e98324f2560af4997e36932becef6836e8
Joseph Hensersky <josephhensersky@10-20-42-127.dynapool.wireless.nyu.edu>
2019-04-11 17:11:29 -0400

    minor fix

--- src/app.js
+++ src/app.js
@@ -1,3 +1,5 @@
+
+const express = require("express");
 const app = express();
 require('./db.js');
 const mongoose = require('mongoose');
@@ -16,4 +18,6 @@ res.send("Hello!").end();
 });
 
 
-app.listen(3000);
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {console.log(`Server is listening on ${port}`)});
--- src/db.js
+++ src/db.js
@@ -19,9 +19,22 @@ const Entry = new mongoose.Schema({
 	date: String
 });
 
+let dbconf;
+if (process.env.NODE_ENV === 'PRODUCTION') {
+ // if we're in PRODUCTION mode:
+ const fs = require('fs');
+ const path = require('path');
+ const fn = path.join(__dirname, 'config.json');
+ const data = fs.readFileSync(fn);
 
+ const conf = JSON.parse(data);
+ dbconf = conf.dbconf;
+} else {
+ // if we're not in PRODUCTION mode, then use
+ dbconf = 'mongodb://localhost/mapdiary';
+}
 
 mongoose.model('User', User);
 mongoose.model('Coordinate', Coordinate);
 mongoose.model('Entry', Entry);
-mongoose.connect('mongodb://localhost/mapdiary');
+mongoose.connect(dbconf);
